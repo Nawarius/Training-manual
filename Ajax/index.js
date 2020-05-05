b3648232e64978e0fb4e489ba9f263ad4d839f44
Nawarius <nawarius1993@gmail.com>
2020-05-05 23:31:20 +0300

    Закончил с ajax. Построил все через axios

--- Ajax/index.js
+++ Ajax/index.js
@@ -1,12 +1,61 @@
-let button = document.querySelector("button");
-let input = document.querySelector("input");
-button.addEventListener('click', () => {getImages(input.value, successData)});
+let button = document.getElementById("button");
+let input = document.getElementById("input");
 
-function successData (data) {
-		data.forEach (el => {
+button.addEventListener('click', () => {
+	getImagePromise(input.value)
+});
+let ID = [];
+
+let taskText = document.getElementById("taskText");
+let pushTaskButton = document.getElementById("taskButton");
+let deleteTaskButton = document.getElementById("deleteButton");
+let getTaskButton = document.getElementById("getTask");
+let numberTask = document.getElementById("numberTask");
+let updateButton = document.getElementById("updateButton");
+
+updateButton.addEventListener('click',()=>{
+	updateTask(numberTask.value,ID[ID.length-1],taskText.value)
+})
+deleteTaskButton.addEventListener('click',()=>{
+	deleteTask(numberTask.value,ID[ID.length-1])
+});
+pushTaskButton.addEventListener('click', () => {
+	pushTask(numberTask.value, taskText.value)
+});
+getTaskButton.addEventListener('click', () => {
+	getTaskPromise(numberTask.value)
+});
+
+function getTaskPromise(id) {
+	const promise = getTask(id);
+	promise.then((responce) => {
+		taskAddHtml(responce)
+	})
+}
+
+function taskAddHtml(text) {
+	let i = 0;
+	text.map(el=>{
+		i==0?i:i++;
+		let task = document.createElement('div');
+		task.innerHTML = el.title; 
+		ID[i] = el.id;
+		document.querySelector('body').appendChild(task);
+	})
+}
+
+
+function getImagePromise(pageNumber) {
+	const promise = getImages(pageNumber);
+	promise.then((responce) => {
+		imagePush(responce);
+	})
+}
+
+function imagePush(data) {
+	data.forEach(el => {
 		let img = document.createElement('img');
 		img.src = el.thumbnail;
 		document.querySelector('body').appendChild(img);
-		}
-	 )
- }
+	})
+}
--- Ajax/service.js
+++ Ajax/service.js
@@ -1,12 +1,37 @@
-
-
-function getImages(input,successData){
-	//alert(input);
-	$.ajax(`https:repetitora.net/api/JS/Images?page=${input}&count=1`, {
-	success: successData
-		}
-	)
+//function getImages(input){
+//	const promise = $.ajax(`https:repetitora.net/api/JS/Images?page=${input}&count=1`);
+//	return promise;
+//}
+function getImages(input){
+	const promise = axios.get(`https:repetitora.net/api/JS/Images?page=${input}&count=1`);
+	return promise.then((responce)=>{
+		return responce.data;
+	})
 }
 
+function getTask(id){
+	//alert(id);
+	const promise = axios.get(`https:repetitora.net/api/JS/Tasks?widgetid=${id}`);
+	return promise.then((responce)=>{
+		return responce.data;
+	})
+}
 
-console.log(5);
+function pushTask(id,text){
+	//alert(id + " " + text)
+	axios.post(`https:repetitora.net/api/JS/Tasks?`, {
+		widgetid:id,
+		title:text
+	});
+}
+
+function deleteTask(widgetid,taskid){
+	axios.delete(`https:repetitora.net/api/JS/Tasks?widgetid=${widgetid}&taskid=${taskid}`);
+}
+function updateTask(id, taskId, text){
+	axios.put(`https:repetitora.net/api/JS/Tasks?`, {
+		widgetid:id,
+		taskid:taskId,
+		title:text
+	});
+}
